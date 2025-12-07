87b6717e92253e4d90c7763a79e358c6916b5f0e
MaxHB01 <67209168+MaxHB01@users.noreply.github.com>
2025-12-07 16:51:19 +0100

    Integrate SignalR live chat support

--- src/common/config/environment.ts
+++ src/common/config/environment.ts
@@ -1,4 +1,10 @@
 export const Environment = {
-	API_URL: process.env.API_URL,
+	API_URL: process.env.NEXT_PUBLIC_API_URL ?? process.env.API_URL,
+	CHAT_HUB_URL:
+		process.env.NEXT_PUBLIC_CHAT_HUB_URL ??
+		process.env.CHAT_HUB_URL ??
+		(process.env.NEXT_PUBLIC_API_URL || process.env.API_URL
+			? `${process.env.NEXT_PUBLIC_API_URL ?? process.env.API_URL}/chatHub`
+			: undefined),
 	TIMEOUT: Number(process.env.TIMEOUT) || 5000,
 };
